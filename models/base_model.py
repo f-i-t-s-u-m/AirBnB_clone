39323739dc5ab96eeaf6f985c920edbddb31c0bd
fitsum belay <fibabirr@gmail.com>
2022-01-29 13:11:04 -0500

    add more dum dum

--- models/base_model.py
+++ models/base_model.py
@@ -2,22 +2,21 @@
 """ base model file """
 
 
-import json, uuid
+import uuid
 from datetime import datetime
 from models import storage
-
 class BaseModel():
-    """ class of base model"""
-   
+    """Base Model"""
     def __init__(self, *args, **kwargs):
-        """ init the class"""
-        if kwargs is not None and  kwargs != {}:
-            for k, v in kwargs.items():
-                if k in ['created_at', 'updated_at']:
-                    self.__dict__[k] = datetime.strptime(str(v), 
-                            '%Y-%m-%dT%H:%M:%S.%f')
-                elif k != '__class__':
-                    setattr(self, k, v)
+        """initialize instance of BaseModel class"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'updated_at':
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key == 'created_at':
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key != '__class__':
+                    setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -25,19 +24,22 @@ class BaseModel():
             storage.new(self)
 
     def __str__(self):
-        """ return str version """
-        return str("[{}] ({}) {}".format(BaseModel.__name__, 
-            self.id, self.__dict__));
+        """class object represented as a string"""
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
 
     def save(self):
-        """ save data"""
+        """updates the public instance attribute 'updated_at'
+        with the current datetime"""
         self.updated_at = datetime.now()
         storage.save()
-    
+
     def to_dict(self):
-        """ return the dict version of the app"""
-        dictm = self.__dict__.copy()
-        dictm['__class__'] = type(self).__name__
-        dictm['created_at'] = dictm['created_at'].isoformat()
-        dictm['updated_at'] = dictm['updated_at'].isoformat()
-        return dictm
+        """returns a dictionary containing all keys/values
+        of __dict__ of the instance"""
+        obj_dict = {}
+        obj_dict.update(self.__dict__)
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict.update({'created_at': obj_dict['created_at'].isoformat()})
+        obj_dict.update({'updated_at': obj_dict['updated_at'].isoformat()})
+        return obj_dict
