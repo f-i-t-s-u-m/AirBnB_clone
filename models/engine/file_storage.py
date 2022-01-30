4b9b4e0fa88e2dc5e1257748b478b768ed0ae64f
fitsum belay <fibabirr@gmail.com>
2022-01-30 05:17:06 -0500

    fix filestorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -15,22 +15,22 @@ class FileStorage:
 
     def all(self):
         """ return objects"""
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """ sets to __objs"""
-        self.__objects = {type(obj).__name__ + "." + obj.id: obj}
+        FileStorage.__objects = {type(obj).__name__ + "." + obj.id: obj}
 
-    def save(self):
+    def save(FileStorage):
         """ serialize _object to json"""
         objFile = {}
         try:
-            with open(self.__file_path) as readData:
+            with open(FileStorage.__file_path) as readData:
                 objFile = json.load(readData)
         except FileNotFoundError:
             pass
-        with open(self.__file_path, "w") as data:
-            obj = {k: v.to_dict() for k, v in self.__objects.items()}
+        with open(FileStorage.__file_path, "w") as data:
+            obj = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
             obj.update(objFile)
             json.dump(obj, data)
 
@@ -48,11 +48,11 @@ class FileStorage:
         """ load or deserilizes json file to _objs"""
         from models.base_model import BaseModel
         try:
-            with open(self.__file_path) as jsonfile:
+            with open(FileStorage.__file_path) as jsonfile:
                 obj = json.load(jsonfile)
                 obj = {k: self.classes()[v['__class__']](**v)
                         for k, v in obj.items()}
-                self.__objects = obj
+                FileStorage.__objects = obj
 
         except FileNotFoundError:
             pass
