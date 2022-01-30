1a625f1342dd442d330f43d5608646b03b7b09b3
fitsum belay <fibabirr@gmail.com>
2022-01-30 04:53:07 -0500

    console 0.0.1

---
+++ console.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Python console file
+"""
+
+import cmd
+import sys
+
+class HBNBCommand(cmd.Cmd):
+    """ class if HBHB command """
+    prompt = '(hbtn) '
+   
+    def do_quite(self, arg):
+        """Quit command to exit the program \n"""
+        return True
+
+    def do_EOF(self, argfs):
+        """ end of file"""
+        return True
+
+    def emptyline(self):
+        """ show nothing"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
+
+
