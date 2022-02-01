7d6c6a82e4e91d3e37af717aa7e232f57d5d9cda
fitsum belay <fibabirr@gmail.com>
2022-02-01 13:08:15 -0500

    done console

--- console.py
+++ console.py
@@ -3,13 +3,50 @@
 Python console file
 """
 import cmd
-import sys
 import re
+import json
 from models import storage 
+
 
 class HBNBCommand(cmd.Cmd):
     """ class if HBHB command """
     prompt = '(hbtn) '
+
+    def default(self, line):
+        """ redirect to _precmd if command not found"""
+        self._precmd(line)
+
+    def _precmd(self, line):
+        """run if no command found"""
+        match = re.search(r"^(\w*)\.(\w+)(?:\(([^)]*)\))$", line)
+        if not match:
+            return line
+        classname = match.group(1)
+        method = match.group(2)
+        args = match.group(3)
+        match_uid_and_args = re.search('^"([^"]*)"(?:, (.*))?$', args)
+        if match_uid_and_args:
+            uid = match_uid_and_args.group(1)
+            attr_or_dict = match_uid_and_args.group(2)
+        else:
+            uid = args
+            attr_or_dict = False
+
+        attr_and_value = ""
+        if method == "update" and attr_or_dict:
+            match_dict = re.search('^({.*})$', attr_or_dict)
+            if match_dict:
+                self.update_dict(classname, uid, match_dict.group(1))
+                return ""
+            match_attr_and_value = re.search(
+                '^(?:"([^"]*)")?(?:, (.*))?$', attr_or_dict)
+            if match_attr_and_value:
+                attr_and_value = (match_attr_and_value.group(
+                    1) or "") + " " + (match_attr_and_value.group(2) or "")
+        command = method + " " + classname + " " + uid + " " + attr_and_value
+        self.onecmd(command)
+        return command
+
     def do_quite(self, arg):
         """Quit command to exit the program \n"""
         return True
@@ -89,7 +126,7 @@ class HBNBCommand(cmd.Cmd):
     
     @findModel
     def do_update(self, args):
-        """ update an instance"""
+        """ update an instance \n"""
         if len(args) == 2:
             print("** attribute name missing **")
             return
@@ -104,6 +141,43 @@ class HBNBCommand(cmd.Cmd):
         data = storage.all()[".".join(args[:2])].__dict__
         data[args[2]] = val
         storage.save()
+
+
+    def update_dict(self, classname, uid, s_dict):
+        """Helper method for update() with a dictionary."""
+        s = s_dict.replace("'", '"')
+        d = json.loads(s)
+        if not classname:
+            print("** class name missing **")
+        elif classname not in storage.classes():
+            print("** class doesn't exist **")
+        elif uid is None:
+            print("** instance id missing **")
+        else:
+            key = "{}.{}".format(classname, uid)
+            if key not in storage.all():
+                print("** no instance found **")
+            else:
+                attributes = storage.attributes()[classname]
+                for attribute, value in d.items():
+                    if attribute in attributes:
+                        value = attributes[attribute](value)
+                    setattr(storage.all()[key], attribute, value)
+                storage.all()[key].save()
+
+    def do_count(self, line):
+        """Counts the instances of a class.
+        """
+        words = line.split(' ')
+        if not words[0]:
+            print("** class name missing **")
+        elif words[0] not in storage.classes():
+            print("** class doesn't exist **")
+        else:
+            matches = [
+                k for k in storage.all() if k.startswith(
+                    words[0] + '.')]
+            print(len(matches))
 
 
 if __name__ == '__main__':
